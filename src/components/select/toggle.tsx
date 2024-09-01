1c0b4334bd86451c88ea7ba92d2c8025e2533b00
denizyoldas <daksu30@gmail.com>
2024-09-01 14:53:39 +0300

    refactor: performance update

--- src/components/select/toggle.tsx
+++ src/components/select/toggle.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useMemo } from "react";
 import { IoIosArrowDown } from "react-icons/io";
 import { cn } from "../../lib/utils";
 import { Option, SelectVariant } from "./model";
@@ -16,81 +16,93 @@ interface ToggleProps {
   setIsOpen: (isOpen: boolean) => void;
 }
 
-const Toggle: React.FC<ToggleProps> = ({
-  isOpen,
-  selectedOption,
-  showItemIcon,
-  icon,
-  searchQuery,
-  setSearchQuery,
-  placeholder,
-  toggleDropdown,
-  setIsOpen,
-  variant,
-}) => {
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
-    if (e.key === "Enter" || e.key === " ") {
-      toggleDropdown();
-    }
-  };
+const Toggle: React.FC<ToggleProps> = React.memo(
+  ({
+    isOpen,
+    selectedOption,
+    showItemIcon,
+    icon,
+    searchQuery,
+    setSearchQuery,
+    placeholder,
+    toggleDropdown,
+    setIsOpen,
+    variant,
+  }) => {
+    const handleKeyDown = useCallback(
+      (e: React.KeyboardEvent<HTMLDivElement>) => {
+        if (e.key === "Enter" || e.key === " ") {
+          toggleDropdown();
+        }
+      },
+      [toggleDropdown]
+    );
 
-  const renderIcon = () => {
-    if (selectedOption && !showItemIcon && selectedOption.img) {
-      return (
-        <img
-          src={selectedOption.img}
-          alt={selectedOption.label}
-          className="w-6 h-6 rounded-full object-cover"
-        />
-      );
-    }
-    return icon ? <div>{icon}</div> : null;
-  };
+    const renderIcon = useMemo(() => {
+      if (selectedOption && !showItemIcon && selectedOption.img) {
+        return (
+          <img
+            src={selectedOption.img}
+            alt={selectedOption.label}
+            className="w-6 h-6 rounded-full object-cover"
+          />
+        );
+      }
+      return icon ? <div>{icon}</div> : null;
+    }, [selectedOption, showItemIcon, icon]);
 
-  const renderInputOrPlaceholder = () => {
-    if (variant === SelectVariant.SEARCH) {
+    const renderInputOrPlaceholder = useMemo(() => {
+      if (variant === SelectVariant.SEARCH) {
+        return (
+          <input
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            placeholder="Search"
+            className="w-full border-none outline-none"
+            onClick={(e) => e.stopPropagation()} // Prevent input click from toggling dropdown
+            onFocus={() => setIsOpen(true)}
+          />
+        );
+      }
+
       return (
-        <input
-          type="text"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          placeholder="Search"
-          className="w-full border-none outline-none"
-          onClick={(e) => e.stopPropagation()} // Prevent input click from toggling dropdown
-          onFocus={() => setIsOpen(true)}
-        />
+        <span className={cn({ "text-placeholder": !selectedOption })}>
+          {selectedOption ? selectedOption.label : placeholder}
+        </span>
       );
-    }
+    }, [
+      variant,
+      searchQuery,
+      setSearchQuery,
+      selectedOption,
+      placeholder,
+      setIsOpen,
+    ]);
 
     return (
-      <span className={cn({ "text-placeholder": !selectedOption })}>
-        {selectedOption ? selectedOption.label : placeholder}
-      </span>
-    );
-  };
-
-  return (
-    <div
-      className={cn(
-        "flex items-center justify-between gap-x-2 border border-secondary rounded-lg px-3 py-2",
-        {
-          "border-focus shadow-focus": isOpen,
-        }
-      )}
-      tabIndex={0}
-      role="button"
-      onClick={toggleDropdown}
-      onKeyDown={handleKeyDown}
-    >
-      <div className="flex items-center gap-x-2 w-full">
-        {renderIcon()}
-        {renderInputOrPlaceholder()}
+      <div
+        className={cn(
+          "flex items-center justify-between gap-x-2 border border-secondary rounded-lg px-3 py-2",
+          {
+            "border-focus shadow-focus": isOpen,
+          }
+        )}
+        tabIndex={0}
+        role="button"
+        onClick={toggleDropdown}
+        onKeyDown={handleKeyDown}
+      >
+        <div className="flex items-center gap-x-2 w-full">
+          {renderIcon}
+          {renderInputOrPlaceholder}
+        </div>
+        <span>
+          <IoIosArrowDown className="text-placeholder" />
+        </span>
       </div>
-      <span>
-        <IoIosArrowDown className="text-placeholder" />
-      </span>
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default Toggle;
